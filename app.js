b47e8366ae5ec87d1fdc98e2038448e7c189e49a
Jan Paul Schnabel <jpschnabel96@gmail.com>
2022-11-10 09:16:12 -0500

    finished lab

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,6 +18,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ...
 
 // Add the route handlers here:
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      console.log(responseFromAPI);
+      res.render('random-beer.hbs', responseFromAPI[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers.hbs', { beersFromApi }))
+    .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
